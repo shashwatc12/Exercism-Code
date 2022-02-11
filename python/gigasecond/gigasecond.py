876d01af086f59d2977b3c917ec0208c272f5f93
Shashwat Chavan <shashwatchavan@SCMac.local>
2022-02-11 17:23:47 -0500

    solved gigacode logic

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,13 @@
+from datetime import date, timedelta
+import datetime
+from sqlite3 import Date
+from xmlrpc.client import DateTime
+
 def add(moment):
-    pass
+    """
+    Given a moment, determine the moment that would be after a gigasecond has passed.
+    A gigasecond is 10^9 (1,000,000,000) seconds.
+    """
+
+    t1=datetime.seconds(10**9)
+    return t1+timedelta(moment)
